3174c8abe7eb1d25f1f6937d6f613eb31958185d
RitwickRaj78 <ritwickraj78@gmail.com>
2020-04-29 15:18:46 +0530

    Init Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+const theme = createMuiTheme();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <MuiThemeProvider theme={theme}>
+    <App/>
+  </MuiThemeProvider>,
+  document.getElementById("root")   
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
