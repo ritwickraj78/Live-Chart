9de86bb69f31e6b1f7fff6605fa37147ffa24ab8
RitwickRaj78 <ritwickraj78@gmail.com>
2020-04-29 15:31:09 +0530

    Updates

---
+++ precache-manifest.0d574ab16c3357abab39a5a26841f9a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb6192c7fd3fed0ab24ee7dd68f5ffa",
+    "url": "/Live-Chart/index.html"
+  },
+  {
+    "revision": "5d34b10800fd234a37b9",
+    "url": "/Live-Chart/static/js/2.bf8b6feb.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/Live-Chart/static/js/2.bf8b6feb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c104d3eb69fe768aaec6",
+    "url": "/Live-Chart/static/js/main.0896f215.chunk.js"
+  },
+  {
+    "revision": "6b86ec503e15d60bf8be",
+    "url": "/Live-Chart/static/js/runtime-main.836a5bc6.js"
+  }
+]);
